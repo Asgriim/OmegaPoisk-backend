da3f5e984f8deb9b609f2aeea8b09c6c98c7695a
Asgriim <andrew.vlasenkov70@gmail.com>
2023-12-27 03:13:43 +0300

    add user search by title

--- src/main/java/org/omega/omegapoisk/controller/SearchController.java
+++ src/main/java/org/omega/omegapoisk/controller/SearchController.java
@@ -1,7 +1,12 @@
 package org.omega.omegapoisk.controller;
 
+import org.omega.omegapoisk.entity.*;
+import org.omega.omegapoisk.service.SearchService;
 import org.omega.omegapoisk.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +16,34 @@ public class SearchController {
 
     @Autowired
     UserService userService;
+
+    @Autowired
+    SearchService searchService;
+
+    @GetMapping("/anime/{title}")
+    public ResponseEntity<?> anime(@PathVariable String title) {
+        return ResponseEntity.ok(searchService.searchByTitle(Anime.class,title));
+    }
+
+    @GetMapping("/game/{title}")
+    public ResponseEntity<?> game(@PathVariable String title) {
+        return ResponseEntity.ok(searchService.searchByTitle(Game.class,title));
+    }
+
+    @GetMapping("/movie/{title}")
+    public ResponseEntity<?> movie(@PathVariable String title) {
+        return ResponseEntity.ok(searchService.searchByTitle(Movie.class,title));
+    }
+
+    @GetMapping("/tv_show/{title}")
+    public ResponseEntity<?> TvShow(@PathVariable String title) {
+        return ResponseEntity.ok(searchService.searchByTitle(TvShow.class,title));
+    }
+
+    @GetMapping("/comic/{title}")
+    public ResponseEntity<?> comic(@PathVariable String title) {
+        return ResponseEntity.ok(searchService.searchByTitle(Comic.class,title));
+    }
 
 
 }
--- src/main/java/org/omega/omegapoisk/service/SearchService.java
+++ src/main/java/org/omega/omegapoisk/service/SearchService.java
@@ -1,4 +1,26 @@
 package org.omega.omegapoisk.service;
 
+import org.omega.omegapoisk.data.CardDTO;
+import org.omega.omegapoisk.entity.Content;
+import org.omega.omegapoisk.repository.SearchRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class SearchService {
+
+    @Autowired
+    SearchRepository searchRepository;
+
+    public <T extends Content> List<CardDTO<T>> searchByTitle(Class<T> cl, String title) {
+        String templ = "select * from %s left join content_tags on content_tags.contentid = %s\n" +
+                "                    left join tags ON tags.id = content_tags.tagid\n" +
+                "                    left join avg_rating ON avg_rating.contentid = %s where  lower(%s) ";
+
+        List<CardDTO<T>> cardDTOS = searchRepository.searchByTitle(cl, title, templ);
+        return cardDTOS;
+
+    }
 }
